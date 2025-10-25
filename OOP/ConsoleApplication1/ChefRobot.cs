a012c420d8bca84a1ec5220963b5e7ab578c808f
leyla <esazade.leyla@gmail.com>
2025-10-25 10:25:08 +0330

    ChefRobot encapsulation example

---
+++ OOP/ConsoleApplication1/ChefRobot.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    //Encapsulation
+    class ChefRobot
+    {
+        private string name;
+        private int batteryLevel;
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public int BatteryLevel
+        {
+            get { return batteryLevel; }
+            set
+            {
+                if (value < 0) batteryLevel = 0;
+                else if (value > 100) batteryLevel = 100;
+                else batteryLevel = value;
+            }
+        }
+
+        public ChefRobot()
+        {
+            name = "Test";
+            batteryLevel = 100;
+        }
+        public void Introduce()
+        {
+            Console.WriteLine("Hello, I am " + Name + " and I have " + batteryLevel.ToString() + "% battery remaining.");
+        }
+
+        public void CookFood(string foodName)
+        {
+            Console.WriteLine(Name + " is cooking " + foodName + " for you! Nom nom.");
+            BatteryLevel -= 10;
+        }
+    }
+}
---
+++ OOP/ConsoleApplication1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var robot1 = new ChefRobot();
+            robot1.Introduce();
+            robot1.CookFood("Pizza");
+            robot1.Introduce();
+
+            Console.ReadLine();
+        }
+    }
+}

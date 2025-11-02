8d7c31a18ef6a8b5274ac8d1e66f2f86bcff4773
leyla <esazade.leyla@gmail.com>
2025-11-02 15:27:31 +0330

    add extension example

---
+++ Extension/ConsoleCalculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+            int number1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter second number");
+            int number2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter the operation");
+            string operation = Console.ReadLine().ToUpperInvariant();
+
+            try
+            {
+                var calculator = new calculator();
+                int result = calculator.Calculator(number1, number2, operation);
+                Console.WriteLine("Output is : " + result);
+            }
+            catch(ArgumentNullException ex){
+                Console.WriteLine("operation was not provided. " + ex);
+            }
+            catch (ArgumentOutOfRangeException ex)
+            {
+                Console.WriteLine("the value out of range. " + ex);
+            }
+            catch(Exception ex){
+                Console.WriteLine("Sorry,something went wrong" + ex);
+            }
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Extension/ConsoleCalculator/calculator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleCalculator
+{
+    class calculator
+    {
+        public int Calculator(int num1, int num2, string operation)
+        {
+            if (operation == "/")
+            {
+                return Divide(num1, num2);
+            }
+            else
+            {
+                throw new ArgumentOutOfRangeException("The matematical operator is not supported");
+            }
+        }
+
+        private int Divide(int number, int divisor)
+        {
+            return number / divisor;
+        }
+    }
+}
